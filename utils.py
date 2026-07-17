95c84aba7cc39c14d460874fcc346e1c2d71a170
seemaletoluo260-collab <seemaletoluo260@gmail.com>
2026-07-17 19:45:05 +0300

    Add utility functions for math and greetings

--- utils.py
+++ utils.py
@@ -1,1 +1,18 @@
+def square(n):
+    """Return the square of a number."""
+    return n * n
 
+
+def is_even(n):
+    """Return True if the number is even, False otherwise."""
+    return n % 2 == 0
+
+
+def celsius_to_fahrenheit(c):
+    """Convert Celsius to Fahrenheit using F = (C * 9/5) + 32."""
+    return (c * 9 / 5) + 32
+
+
+def greet(name):
+    """Return a personalized greeting string."""
+    return f"Hello, {name}!"
